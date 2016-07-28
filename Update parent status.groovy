2c95e9375997e4794bab0e30114479508e2d6e54
Chander Inguva <chander.inguva@gmail.com>
2016-07-27 21:46:54 -0500

    Update post-function

--- Update parent status.groovy
+++ Update parent status.groovy
@@ -8,22 +8,17 @@ import com.atlassian.jira.workflow.WorkflowTransitionUtilImpl;
 import com.atlassian.jira.util.JiraUtils;
 import com.atlassian.jira.issue.status.Status
 
-def issue = ComponentAccessor.getIssueManager().getIssueByCurrentKey('SVP-60')
-if (issue.getSummary() == 'Automate back ups')
-
+//def issue = ComponentAccessor.getIssueManager().getIssueByCurrentKey('SVP-60')
+if (issue.getSummary() == 'Organize Directories')
 {
 // Get the details of the user executing the workflow transition for the issue change history
-//String currentUser = ((WorkflowContext) transientVars.get("context")).getCaller();
+String currentUser = ((WorkflowContext) transientVars.get("context")).getCaller();
 WorkflowTransitionUtil workflowTransitionUtil = (WorkflowTransitionUtil) JiraUtils.loadComponent(WorkflowTransitionUtilImpl.class);
- //return 1;
 // Get the ID of the parent issue
 MutableIssue parent = issue.getParentObject() as MutableIssue
 workflowTransitionUtil.setIssue(parent);
-workflowTransitionUtil.setAction(21); // 91 is the ID of the Global Dev In Progress transition
+workflowTransitionUtil.setAction(11); 
+// 11 is the ID of the 	Complete Asset and Metadata Request
 workflowTransitionUtil.validate();
 workflowTransitionUtil.progress(); 
 }
-else
-    {
-        return 0;
-    }
